dc2d5bf28671eeb381884a827204316bccd21c63
sluo6 <shangwenluo6@gmail.com>
2018-05-01 13:28:34 -0500

    working on BST

--- src/BST.java
+++ src/BST.java
@@ -1,37 +1,149 @@
+import java.util.NoSuchElementException;
+
 /**
  * Created by Shangwen on 2018/4/30.
  * Implementation of a generic binary search tree.
+ * Learn to write javadoc.
  */
 public class BST<Key extends Comparable<Key>, Value> implements ST<Key, Value> {
+        private Node root;
+
+        private class Node{
+            private Key key;
+            private Value val;
+            private Node left, right;
+            private int size;    // number of nodes in subtree
+
+            public Node(Key key, Value val, int size) {
+                this.key = key;
+                this.val = val;
+                this.size = size;
+            }
+        }
+
+        /**
+         Initializes an empty symbol table.
+        */
+        public BST(){
+        }
+
+    /**
+     * Inserts the specific key-value pair into the symbol table, overwriting the old
+     * value with the new value if the symbol table already contains the specified key.
+     * Deletes the specified key (and its associated value) from this symbol table
+     * if the specified value is {@code null}.
+     *
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
     @Override
     public void put(Key key, Value val) {
-
+        if (key == null) throw new IllegalArgumentException("calls put() with a null key");
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        root = put(root, key, val);
+        assert check();
     }
 
+    // Insert the specific key-value pair into the symbol table, stem from node x.
+    // Return the link, or pointer, to the new node.
+   private Node put(Node x, Key key, Value val) {
+        // recursion end condition
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+   }
+
+    /**
+     * Returns the value associated with the given key.
+     *
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *         and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
     @Override
     public Value get(Key key) {
-        return null;
+        return get(root, key);
     }
 
+    // Returns the value associated with the given key, stem from Node x
+    private Value get(Node x, Key key) {
+        if (key == null) throw new IllegalArgumentException("calls get() with a null key");
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x.val;
+    }
+
+    /**
+     * Removes the specified key and its associated value from this symbol table
+     * (if the key is in this symbol table).
+     *
+     * @param key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
     @Override
     public void delete(Key key) {
-
+        if (key == null) throw new IllegalArgumentException("calls delete() with a null key");
+        root = delete(root, key);
+        assert check();
     }
 
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            
+        }
+    }
+
+    /** Does this symbol table contain the given key?
+     *
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
     @Override
     public boolean contains(Key key) {
-        return false;
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
     }
 
+    /**
+     * Returns true if this symbol table is empty.
+     * @return {@code true} if this symbol table is empty; {@code false} otherwise
+     */
     @Override
     public boolean isEmpty() {
-        return false;
+            return size() == 0;
     }
 
+    /** Returns the number of key-value pairs in this symbol table.
+     * @return the number of key-value pairs in this symbol table
+     */
     @Override
     public int size() {
-        return 0;
+        return size(root);
     }
+
+    // return number of key-value pairs in BST rooted at x
+   private int size(Node x) {
+        if (x == null) return 0;
+        else return x.size;
+   }
 
     @Override
     public Key min() {
@@ -63,15 +175,42 @@ public class BST<Key extends Comparable<Key>, Value> implements ST<Key, Value> {
         return null;
     }
 
+    /**
+     * Removes the smallest key and associated value from the symbol table.
+     *
+     * @throws java.util.NoSuchElementException if the symbol table is empty
+     */
     @Override
     public void deleteMin() {
-
+        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+        root = deleteMin(root);
+        assert check();
     }
 
+    private Node deleteMin(Node x) {
+        if(x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    /**
+     * Removes the largest key and associated value from the symbol table.
+     *
+     * @throws NoSuchElementException if the symbol table is empty
+     */
     @Override
     public void deleteMax() {
-
+        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+        root = deleteMax(root);
+        assert check();
     }
+     private Node deleteMax(Node x) {
+        if (x.right == null) return x.left;
+        x.right = deleteMax(x.right);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+     }
 
     @Override
     public int size(Key lo, Key hi) {
