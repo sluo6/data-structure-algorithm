b4ea912a4e4aaa8873c9d6562032105b86c29ee0
sluo6 <shangwenluo6@gmail.com>
2018-05-18 13:59:59 -0500

    finished RedBlackBST

--- src/sluo/learn/algorithm/RedBlackBST.java
+++ src/sluo/learn/algorithm/RedBlackBST.java
@@ -1,6 +1,8 @@
 package sluo.learn.algorithm;
 
 import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * Created by Shangwen on 2018/5/02.
@@ -153,7 +155,27 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> implements ST<Key, 
      */
     @Override
     public void deleteMin() {
+        if (isEmpty()) throw new NoSuchElementException("BST underflow");
 
+        // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
+        root = deleteMin(root);
+        if (!isEmpty()) root.color = BLACK;
+
+    }
+
+    // delete the key-value pair with the minimum key rooted at h
+    private Node deleteMin(Node h) {
+        if (h.left == null)
+            return null;
+
+        if (!isRed(h.left) && !isRed(h.left.left))
+            h = moveRedLeft(h);
+
+        h.left = deleteMin(h.left);
+        return balance(h);
     }
 
     /**
@@ -161,8 +183,32 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> implements ST<Key, 
      */
     @Override
     public void deleteMax() {
+        if (isEmpty()) throw new NoSuchElementException("BST underflow");
 
+        // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+        root.color = RED;
+
+        root = deleteMax(root);
+        if (!isEmpty()) root.color = BLACK;
     }
+
+    // delete the key-value pair with the maximum key rooted at h
+    private Node deleteMax(Node h) {
+        if (isRed(h.left))
+            h = rotateRight(h);
+
+        if (h.right == null)
+            return null;
+
+        if (!isRed(h.right) && !isRed(h.right.left))
+            h = moveRedRight(h);
+
+        h.right = deleteMax(h.right);
+
+        return balance(h);
+    }
+
 
     /**
      * Remove key and its value from table.
@@ -171,7 +217,40 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> implements ST<Key, 
      */
     @Override
     public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+        if (!contains(key)) return;
 
+        // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
+        root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    // delte the key-value pair with the given key rooted at h
+    private Node delete(Node h, Key key) {
+        if (key.compareTo(h.key) < 0) {
+            if (!isRed(h.left) && !isRed(h.left.left))
+                h = moveRedLeft(h);
+            h.left = delete(h.left, key);
+        }
+        else {
+            if (isRed(h.left))
+                h = rotateRight(h);
+            if (key.compareTo(h.key) == 0 && (h.right == null))
+                return null;
+            if (!isRed(h.right) && !isRed(h.right.left))
+                h = moveRedRight(h);
+            if (key.compareTo(h.key) == 0) {
+                Node x = min(h.right);
+                h.key = x.key;
+                h.val = x.val;
+                h.right = delteMin(h.right);
+            }
+            else h.right = delete(h.right, key);
+        }
+        return balance(h);
     }
 
     /***********************************************
@@ -206,15 +285,36 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> implements ST<Key, 
         h.right.color = !h.right.color;
     }
 
+    // Assuming that h is red and both h.left and h.left.left are black,
+    // make h.left or one of its children red.
     private Node moveRedLeft(Node h) {
+        flipColors(h);
+        if(isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            flipColors(h);
+        }
         return h;
     }
 
+    // Assuming that h is red and both h.right and h.right.left are black,
+    // make h.right or one of its children red.
     private Node moveRedRight(Node h) {
+        flipColors(h);
+        if(isRed(h.left.left)) {
+            h = rotateRight(h);
+            flipColors(h);
+        }
         return h;
     }
 
+    // restore red-black tree invariant
     private Node balance(Node h) {
+        if(isRed(h.right))      h = rotateLeft(h);
+        if(isRed(h.left) && isRed(h.left.left))      h = rotateRight(h);
+        if(isRed(h.left) && isRed(h.right))          flipColors(h);
+
+        h.size = size(h.left) + size(h.right) + 1;
         return h;
     }
 
@@ -293,27 +393,51 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> implements ST<Key, 
     @Override
     public Key ceiling(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to ceiling() is null");
-        if (isEmpty()) throw new 
+        if (isEmpty()) throw new
     }
 
-    /**
-     * Number of keys less than key.
-     *
-     * @param key
-     */
+    // the smallest key in the subtree rooted at x greater than or equal to the given key
+    private Node ceiling(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp > 0) return ceiling(x.right, key);
+        Node t = ceiling(x.left, key);
+        if (t != null) return t;
+        else return x;
+    }
+
+
     @Override
     public int rank(Key key) {
-        return 0;
+        if (key == null) throw new IllegalArgumentException("argument to rank(0 is null");
+        return rank(key, root);
     }
 
-    /**
-     * Key of rank k.
-     *
-     * @param k
-     */
+    // number of keys less than key in the subtree rooted at x
+    private int rank(Key key, Node x) {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return rank(key, x.left);
+        else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right);
+        else return size(x.left);
+    }
+
     @Override
     public Key select(int k) {
-        return null;
+        if (k < 0 || k >= size()) {
+            throw new IllegalArgumentException("argument to select() is invalid: " + k);
+        }
+        Node x = select(root, k);
+        return x.key;
+    }
+
+    // the key of rank k in the subtree rooted at x
+    private Node select(Node x, int k) {
+        int t = size(x.left);
+        if (t > k) return select(x.left, k);
+        else if (t < k) return select(x.right, k-t-1);
+        else return x;
     }
 
 
@@ -325,8 +449,17 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> implements ST<Key, 
      */
     @Override
     public int size(Key lo, Key hi) {
-        return 0;
+        if (lo == null) throw new IllegalArgumentException("first argument to size() is null");
+        if (hi == null) throw new IllegalArgumentException("second argument to size() is null");
+
+        if (lo.compareTo(hi) > 0) return 0;
+        if (contains(hi)) return rank(hi) - rank(lo) + 1;
+        else return rank(hi) - rank(lo);
     }
+
+    /********************************************************
+     *   Range count and range search.
+     ********************************************************/
 
     /**
      * Keys in range [lo..hi], in sorted order.
@@ -336,7 +469,22 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> implements ST<Key, 
      */
     @Override
     public Iterable<Key> keys(Key lo, Key hi) {
-        return null;
+        if (lo == null) throw new IllegalArgumentException("first argument to keys() is null");
+        if (hi == null) throw new IllegalArgumentException("second argunent to keys() is null");
+
+        PriorityQueue<Key> queue = new PriorityQueue<>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    // add the keys between lo and hi in the subtree rooted at x to the queue
+    private void keys(Node x, PriorityQueue<Key> queue, Key lo, Key hi) {
+        if (x == null) return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) keys(x.left, queue, lo, hi);
+        if (cmplo <= 0 && cmphi >= 0) queue.add(x.key);
+        if (cmphi > 0) keys(x.right, queue, lo, hi);
     }
 
     /**
@@ -344,6 +492,71 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> implements ST<Key, 
      */
     @Override
     public Iterable<Key> keys() {
-        return null;
+        if (isEmpty()) return new PriorityQueue<Key>();
+        return keys(min(), max());
+    }
+
+    /*********************************************************
+     *   Check integrity of red-black tree data structure.
+     ********************************************************/
+    private boolean check() {
+        if (!isBST()) System.out.println("Not in symmetric order");
+        if (!isSizeConsistent()) System.out.println("Subtree counts not consistent");
+        if (!isRankConsistent()) System.out.println("Ranks not consistent");
+        if (!is23()) System.out.println("Not a 2-3 tree");
+        if (!isBalanced()) System.out.println("Not balanced");
+        return isBST() && isSizeConsistent() && isRankConsistent() && is23() && isBalanced();
+    }
+
+    private boolean isBST() {
+        return isBST(root, null, null);
+    }
+
+    private boolean isBST(Node x, Key min, Key max) {
+        if (x == null) return true;
+        if (min != null && x.key.compareTo(min) <= 0) return false;
+        if (max != null && x.key.compareTo(max) >= 0) return false;
+        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+    }
+
+    private boolean isSizeConsistent() {return isSizeConsistent(root);}
+    private boolean isSizeConsistent(Node x) {
+        if (x == null) return true;
+        if (x.size != size(x.left) + size(x.right) + 1) return false;
+        return isSizeConsistent(x.left) && isSizeConsistent(x.right);
+    }
+
+    private boolean isRankConsistent() {
+        for (int i = 0; i < size(); i++)
+            if (i != rank(select(i))) return false;
+        for (Key key : keys())
+            if (key.compareTo(select(rank(key))) != 0) return false;
+        return true;
+    }
+
+    private boolean is23() { return is23(root); }
+    private boolean is23(Node x) {
+        if (x == null) return true;
+        if (isRed(x.right)) return false;
+        if (x != root && isRed(x) && isRed(x.left))
+            return false;
+        return is23(x.left) && is23(x.right);
+    }
+
+    private boolean isBalanced() {
+        int black = 0;
+        Node x = root;
+        while (x != null) {
+            if (!isRed(x)) black++;
+            x = x.left;
+        }
+        return isBalanced(root, black);
+    }
+
+    // does every path from the root to a leaf have the given number of black links?
+    private boolean isBalanced(Node x, int black) {
+        if (x == null) return black == 0;
+        if (!isRed(x)) black--;
+        return isBalanced(x.left, black) && isBalanced(x.right, black);
     }
 }
